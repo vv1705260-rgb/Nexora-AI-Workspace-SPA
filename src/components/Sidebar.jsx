4f9504ba5b1b6b3216e21c1de33bcc59dadce37a
Vaishali <vv1705260@gmail.com>
2026-05-14 15:17:56 +0530

    Create Sidebar.jsx

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import React
+from "react";
+
+import {
+    NavLink
+}
+from "react-router-dom";
+
+/* =========================================
+   ICONS
+========================================= */
+
+import {
+
+    FaHome,
+    FaUserAstronaut,
+    FaChartPie,
+    FaEnvelope,
+    FaRocket,
+    FaBrain,
+    FaLayerGroup,
+    FaGear,
+    FaArrowTrend
